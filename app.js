4b85b885a6a00dc9e9385b614809e92109bd312e
alexHoggett <alexmh99@gmail.com>
2022-02-05 09:00:07 +0000

    updated to css variables

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const allSections = document.querySelector('.section');
 const navLinks = document.querySelector('.topnav__list');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 
-navLinks.addEventListener('click', function(e) {
+navLinks.addEventListener('click', function (e) {
     e.preventDefault();
 
-    if (e.target.classList.contains('topnav__link')){
+    if (e.target.classList.contains('topnav__link')) {
         document.querySelector(e.target.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     }
 });
 
-btnScrollTo.addEventListener('click', function(e) {
+btnScrollTo.addEventListener('click', function (e) {
     const section1 = document.querySelector('#section--1');
     section1.scrollIntoView({
         behavior: 'smooth'
